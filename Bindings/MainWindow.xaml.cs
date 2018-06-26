1506cf1e0e8cfac9b90e91c6bf30402c39e6c000
jaggedSpire <JS@Windows-Vivobook>
2018-06-25 21:50:19 -0500

    Move bindings to dedicated DataContext class Fix #7

--- Bindings/MainWindow.xaml.cs
+++ Bindings/MainWindow.xaml.cs
@@ -7,58 +7,16 @@ namespace Bindings
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-
-        private int _leftHandSide = 1;
-        private int _rightHandSide = 2;
-        private int _sum = 3;
+        private SumData _sumData;
 
         public MainWindow()
         {
             InitializeComponent();
+            _sumData = new SumData();
+            DataContext = _sumData;
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnNotifyPropertyChanged([CallerMemberName]string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-
-        public int LeftHandSide
-        {
-            get { return _leftHandSide; }
-            set
-            {
-                if (_leftHandSide == value) { return; }
-                _leftHandSide = value;
-                OnNotifyPropertyChanged();
-                Sum = LeftHandSide + RightHandSide;
-            }
-        }
-
-        public int RightHandSide
-        {
-            get { return _rightHandSide; }
-            set
-            {
-                if (_rightHandSide == value) { return; }
-                _rightHandSide = value;
-                OnNotifyPropertyChanged();
-                Sum = LeftHandSide + RightHandSide;
-            }
-        }
-
-        public int Sum
-        {
-            get { return _sum; }
-            private set
-            {
-                if (_sum == value) { return; }
-                _sum = value;
-                OnNotifyPropertyChanged();
-            }
-        }
     }
 }
---
+++ Bindings/SumData.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Bindings
+{
+    /// <summary>
+    /// It's some data, geddit? haha
+    /// model for summing properties
+    /// </summary>
+    class SumData : INotifyPropertyChanged
+    {
+        private int _leftHandSide = 1;
+        private int _rightHandSide = 2;
+        private int _sum = 3;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnNotifyPropertyChanged([CallerMemberName]string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public int LeftHandSide
+        {
+            get { return _leftHandSide; }
+            set
+            {
+                if (_leftHandSide == value) { return; }
+                _leftHandSide = value;
+                OnNotifyPropertyChanged();
+                Sum = LeftHandSide + RightHandSide;
+            }
+        }
+
+        public int RightHandSide
+        {
+            get { return _rightHandSide; }
+            set
+            {
+                if (_rightHandSide == value) { return; }
+                _rightHandSide = value;
+                OnNotifyPropertyChanged();
+                Sum = LeftHandSide + RightHandSide;
+            }
+        }
+
+        public int Sum
+        {
+            get { return _sum; }
+            private set
+            {
+                if (_sum == value) { return; }
+                _sum = value;
+                OnNotifyPropertyChanged();
+            }
+        }
+
+    }
+}
