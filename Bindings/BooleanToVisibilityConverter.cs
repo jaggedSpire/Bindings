ae4b5bf5e328ab66f2548b36cd87757c97dd5ae5
jaggedSpire <JS@Windows-Vivobook>
2018-06-29 23:58:21 -0500

    use more standard naming convention for bool -> visibility converter

--- Bindings/BooleanToVisibilityConverter.cs
+++ Bindings/BooleanToVisibilityConverter.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace Bindings
 {
-    class VisibilityConverter : IValueConverter
+    class BooleanToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
